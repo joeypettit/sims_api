5576b1946c74f42f1536f71b8a2f8d5465332bdf
Joey Pettit <pettitbass@gmail.com>
2024-11-04 13:13:55 -0600

    added routes for gettings and posting units

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express, { Request, Response } from "express";
 import projectsRoutes from "./routes/projects";
 import lineItemsRoutes from "./routes/line-items";
 import groupRoutes from "./routes/groups";
-import templateRoutes from "./routes/templates";
+import templatesRoutes from "./routes/templates";
+import unitsRoutes from "./routes/units";
 
 const app = express();
 const port = 3000;
@@ -11,7 +12,8 @@ app.use(express.json());
 app.use("/api/projects", projectsRoutes);
 app.use("/api/line-items", lineItemsRoutes);
 app.use("/api/groups", groupRoutes);
-app.use("/api/templates", templateRoutes);
+app.use("/api/templates", templatesRoutes);
+app.use("/api/units", unitsRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
---
+++ src/routes/units.ts
@@ -0,0 +1,45 @@
+import express from "express";
+const router = express.Router();
+import prisma from "../../prisma/prisma-client";
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await prisma.lineItemUnit.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+    console.log("Units", result);
+    res.status(200).send(result);
+  } catch (error) {
+    console.error("Error fetching units:", error);
+    res.status(500).json({ error: "An error occurred while fetching units." });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { unitName } = req.body;
+
+  if (!unitName) {
+    res.status(400).json({ error: "Unit name is required" });
+    return;
+  }
+
+  try {
+    const newUnit = await prisma.lineItemUnit.create({
+      data: {
+        name: unitName,
+      },
+    });
+
+    res.status(201).json(newUnit);
+  } catch (error) {
+    console.error("Error creating new unit:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating new unit" });
+  }
+});
+
+export default router;
