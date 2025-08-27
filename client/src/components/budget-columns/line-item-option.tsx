00ebd1c32e09fd886b17ad9656f8a732c14b94f5
Joey Pettit <joeypettit@MacBookPro.midcoip.net>
2025-08-27 00:12:05 -0500

    added unit cost to line item option when quantity is zero, other small styling tweaks

--- client/src/components/budget-columns/line-item-option.tsx
+++ client/src/components/budget-columns/line-item-option.tsx
@@ -3,6 +3,7 @@ import IsCheckedIcon from "../is-checked-icon";
 import type { LineItem } from "../../app/types/line-item";
 import {
   getOptionsTotalSalePrice,
+  getOptionsPerUnitSalePrice,
   formatNumberWithCommas,
 } from "../../util/utils";
 
@@ -37,6 +38,31 @@ export default function LineItemOptionDisplay({
     return `$${lowPrice} - $${highPrice}`;
   }
 
+  function getDisplayedPerUnitPrice() {
+    const perUnitPrice = getOptionsPerUnitSalePrice({
+      option: props.lineItemOption,
+      lineItem: props.lineItem,
+    });
+    
+    if (typeof perUnitPrice == "number") {
+      return `$${perUnitPrice}`;
+    }
+    if (perUnitPrice.lowPriceInDollars == perUnitPrice.highPriceInDollars) {
+      return `$${perUnitPrice.lowPriceInDollars}`;
+    }
+    const lowPrice = formatNumberWithCommas(perUnitPrice.lowPriceInDollars);
+    const highPrice = formatNumberWithCommas(perUnitPrice.highPriceInDollars);
+    return `$${lowPrice} - $${highPrice}`;
+  }
+
+  function getBackgroundClass() {
+    if (!props.lineItemOption.isSelected) return "";
+    
+    return props.lineItem.quantity === 0 
+      ? "bg-stone-100 shadow-inner" 
+      : "bg-sims-green-50 shadow-inner";
+  }
+
   return (
     <div
       onClick={() =>
@@ -44,24 +70,35 @@ export default function LineItemOptionDisplay({
           optionToSelect: props.lineItemOption,
         })
       }
-      className={`cursor-pointer hover:shadow-inner relative text-center rounded-sm p-3 ${
-        props.lineItemOption.isSelected ? "bg-sims-green-50 shadow-inner" : ""
-      }`}
+      className={`cursor-pointer hover:shadow-inner relative text-center rounded-sm p-3 ${getBackgroundClass()}`}
     >
       <div className="absolute right-1 top-1">
         <IsCheckedIcon
           isChecked={props.lineItemOption.isSelected}
           iconSize="1rem"
+          forceGrey={props.lineItem.quantity === 0}
         />
       </div>
-      <p
-        className={`text-sm ${
-          props.lineItemOption.isSelected ? "font-bold" : "font-normal"
-        }`}
-      >
-        {getDisplayedPrice()}
-      </p>
-      <p className="text-stone-600 text-xs">
+      {props.lineItem.quantity === 0 ? (
+        <div className="text-center">
+          <p className={`text-sm ${props.lineItemOption.isSelected ? "font-bold" : "font-normal"} text-gray-500`}>
+            {getDisplayedPerUnitPrice()} / unit
+          </p>
+          <p className="text-xs text-gray-500">
+            
+          </p>
+        </div>
+      ) : (
+        <p
+          className={`text-sm ${
+            props.lineItemOption.isSelected ? "font-bold" : "font-normal"
+          }`}
+        >
+          {getDisplayedPrice()}
+        </p>
+      )}
+      <hr className="border-gray-200 my-1 mx-10" />
+      <p className={`text-xs ${props.lineItem.quantity === 0 ? "text-gray-500" : "text-stone-600"}`}>
         {props.lineItemOption.description}
       </p>
     </div>
--- client/src/components/is-checked-icon.tsx
+++ client/src/components/is-checked-icon.tsx
@@ -4,22 +4,24 @@ import { FaCircleCheck } from "react-icons/fa6";
 type CheckOrEmptyIconProps = {
   isChecked: boolean;
   iconSize: string;
+  forceGrey?: boolean;
 };
 
 export default function IsCheckedIcon({
   isChecked,
   iconSize,
+  forceGrey = false,
 }: CheckOrEmptyIconProps) {
   return (
     <div className="flex items-center justify-center text-2xl">
       {isChecked ? (
-        <span className="text-sims-green-900">
+        <span className={forceGrey ? "text-stone-400" : "text-sims-green-900"}>
           <FaCircleCheck size={iconSize} />
-        </span> // Checkmark
+        </span>
       ) : (
         <span className="text-gray-400">
           <FaRegCircle size={iconSize} />
-        </span> // Empty circle
+        </span>
       )}
     </div>
   );
